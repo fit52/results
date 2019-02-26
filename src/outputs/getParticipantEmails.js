1447b648dff1d91ca2e17a25864d532e0a67364c
Ben Vardy <ben@vardyfamily.co.uk>
2019-02-26 20:11:51 +0000

    Tidy up console outputs

--- src/outputs/getParticipantEmails.js
+++ src/outputs/getParticipantEmails.js
@@ -7,6 +7,7 @@
 
 const fs = require('fs');
 const minimist = require('minimist');
+const path = require('path');
 const { db } = require('../connect')();
 
 const argv = minimist(process.argv.slice(2));
@@ -15,6 +16,8 @@ if (argv._.length === 0) throw new Error('No Event provided');
 
 const { _: [ eventNo ] } = argv;
 if (!/\d+/.test(eventNo)) throw new Error('Invalid Event provided');
+
+const dirPath = path.normalize(`${__dirname}/../../secrets/output-data/event${eventNo}`);
 
 db.find({
     selector: {
@@ -32,14 +35,13 @@ db.find({
 .then(({ docs }) => {
     let out = docs.map(email => email.email );
 
-    const dirPath = `${__dirname}/../../secrets/output-data/event${eventNo}`;
     fs.mkdir(dirPath, { recursive: true }, err => {
         if (err && err.code !== 'EEXIST'){
             console.error(err);
         } else {
-            fs.writeFileSync(dirPath + '/emails.csv', out.join('\n'));
+            fs.writeFileSync(path.join(dirPath, 'emails.csv'), out.join('\n'));
         }
     });
 })
-.then(() => console.log('Done!'))
+.then(() => console.log(`Output to ${path.join(dirPath, 'emails.csv' )}`))
 .catch(err => console.error(err.message));
